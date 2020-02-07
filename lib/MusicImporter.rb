78c904306835169ef33918a4a7f0a21cf1d47500
Diego Vasquez <botellongo69@outlook.com>
2020-02-07 19:17:33 +0000

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,13 +1,20 @@
-class MusicLibraryController
-  
-  def list_songs()
+class MusicImporter
 
-    @all_songs = Song.all
-    songs_sorted_by_name = @all_songs.sort_by do |song|
-      song.name
+  def initialize(path)
+    @path = path
+  end
+
+  def path
+    @path
+  end
+
+  def files()
+    @files ||= Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
+  end
+
+  def import()
+    files.each do |filename|
+      Song.create_from_filename(filename)
     end
-    songs_sorted_by_name.each.with_index(1) do |song,index|
-          puts "#{index}. #{song.artist} - #{song.name} - #{song.genre}"
-        end
   end
 end
