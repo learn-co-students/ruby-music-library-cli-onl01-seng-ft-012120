8197011f3416d5c600e68631196cd37f7435c90f
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-09 17:28:12 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,6 +4,8 @@ module Concerns::Findable
     self.all.detect {|x| x.name == name}
   end
   
-  def find
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
   
 end
