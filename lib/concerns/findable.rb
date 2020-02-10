3170e8834eca08269d6622c2e84dc03cf2ed96d4
Kyle McKenna <mckennakyler@gmail.com>
2020-02-10 06:24:56 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,3 +1,9 @@
 module Concerns::Findable
-  
+  def find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  # def self.find_by_name(name)
+  #   self.all.detect{|song| song.name == name}
+  # end   
+  end 
+
 end   
