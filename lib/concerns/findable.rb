77498c6840b87a88a1efc4b539da7f27fe1900e0
Edwin Cyr <edmundopierre@yahoo.com>
2020-02-27 18:12:07 +0000

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,11 @@
+# require 'bundler'
+# Bundler.require
+
+# require_relative "../lib/song.rb"
+# require_relative "../lib/artist.rb"
+# require_relative "../lib/genre.rb"
+
+# module Concerns
+# end
+
+# require_all 'lib'
